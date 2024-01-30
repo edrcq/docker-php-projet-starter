dc894d47cfed131489e3589537a6b4d9085600ba
Edouard Rucquoy <erucquoy@gmail.com>
2024-01-30 11:47:09 +0100

    add bootstrap and some stuff

--- public/index.php
+++ public/index.php
@@ -6,13 +6,23 @@ require_once __DIR__ . '/../src/init.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="/style.css">
+    <title>Bonjour</title>
+    <?php require_once __DIR__ . '/../src/partials/head_css.php'; ?>
 </head>
 <body>
     <?php require_once __DIR__ . '/../src/partials/menu.php'; ?>
     <?php require_once __DIR__ . '/../src/partials/show_error.php'; ?>
 
-    <h1>Bonjour</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Bonjour</h1>
+                <div class="alert alert-success">
+                    Bienvenue sur la boutique !
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
