f487ebfdfc452b4d9a48227e7e90efa150b77616
Edouard Rucquoy <erucquoy@gmail.com>
2024-01-30 11:40:30 +0100

    All in one starter

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../src/init.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <?php require_once __DIR__ . '/../src/partials/menu.php'; ?>
+    <?php require_once __DIR__ . '/../src/partials/show_error.php'; ?>
+
+    <h1>Bonjour</h1>
+</body>
+</html>
